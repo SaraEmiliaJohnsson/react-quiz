7babbba87eccca624543110d72341771e62eea5c
Sara Johnsson <sara86persson@hotmail.com>
2024-04-20 16:27:40 +0200

    added useState and switch case

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,35 @@ import Welcome from './components/Welcome';
 import Game from './components/Game';
 import Result from './components/Result';
 
-
+enum Screen {
+	WELCOME = 'welcome',
+	GAME = 'game',
+	RESULT = 'result'
+}
 
 function App() {
 
-	const [currentScreen, setCurrentScreen] = useState();
+	const [currentScreen, setCurrentScreen] = useState<Screen>(Screen.WELCOME);
+
+	let content: React.ReactElement | null = null
+
+	switch (currentScreen) {
+		case Screen.WELCOME:
+			content = <Welcome />
+			break;
+		case Screen.GAME:
+			content = <Game />
+			break;
+		case Screen.RESULT:
+			content = <Result />
+			break;
+		default:
+			content = null;
+	}
 
 	return (
 		<>
-			<Welcome />
-			<Game />
-			<Result />
+			{content}
 		</>
 	);
 }
